ff0ade46e88fc12b557956d6e01fb55c6c59cca3
Hoa Vu Thu <tinka.vuthu@gmail.com>
2020-05-10 00:17:05 +0200

    doc2vec model
    doc2vec model trained on OPTIMED and NZIP, tested on DRG

--- cmdParamReadingExample.py
+++ cmdParamReadingExample.py
@@ -5,12 +5,14 @@ from scipy import spatial
 from def_udpipe import UDPipe_preprocessing_word
 from def_sentence_preprocessing import sentence_preprocessing
 
-firstArg = sys.argv[1]
-secondArg = sys.argv[2]
+Arg_1 = sys.argv[1]
+Arg_2 = sys.argv[2]
 
+#Arg_1 = "srdeční nemoc"
+#Arg_2  = "infarkt"
 
-preproc_1 = sentence_preprocessing([firstArg])
-preproc_2 = sentence_preprocessing([secondArg])
+preproc_1 = sentence_preprocessing([Arg_1])
+preproc_2 = sentence_preprocessing([Arg_2])
 
 UDPipe_1 = [UDPipe_preprocessing_word(ls) for ls in preproc_1]
 UDPipe_2 = [UDPipe_preprocessing_word(ls) for ls in preproc_2]
@@ -18,18 +20,19 @@ UDPipe_2 = [UDPipe_preprocessing_word(ls) for ls in preproc_2]
 str_1 = " ".join(UDPipe_1)
 str_2 = " ".join(UDPipe_2)
 
+sentence_1 = str_1.split()
+sentence_2 = str_2.split()
 ### doc2vec
+model = doc2vec.Doc2Vec.load('doc2vec_optimed_NZIP.model')
 
-model = doc2vec.Doc2Vec.load('doc2vec_optimed.model')
 model.random.seed(0)
-
-vec1 =  model.infer_vector(UDPipe_1)
-vec2 =  model.infer_vector(UDPipe_2)
+vec1 =  model.infer_vector(sentence_1)
+vec2 =  model.infer_vector(sentence_2)
 
 cos = 1 - spatial.distance.cosine(vec1, vec2)
 similarity = round(((cos + 1)/2)*100, 2) 
 
-print ("First Argument: " + str_1)
-print ("Second Argument: " + str_2)
-print ("Cosinus sim: " + str(cos))
-print (similarity)
+print("First Argument: " + str_1)
+print("Second Argument: " + str_2)
+print("Cosinus similarity: " + str(cos))
+print("Cosinus similarity ranged from 0 to 1: " + str(similarity))
--- doc2vec.py
+++ doc2vec.py
@@ -8,7 +8,7 @@ def create_tagged_document(list_of_list_of_words):
     for i, list_of_words in enumerate(list_of_list_of_words):
         yield gensim.models.doc2vec.TaggedDocument(list_of_words, [i])
         
-with open('OPTIMED_clean_preprocessed.txt', 'r', encoding = 'utf8') as file:
+with open('OPTIMED_NZIP_clean.txt', 'r', encoding = 'utf8') as file:
     train_corpus = json.load(file)
 
 train_corpus= [list(ls.split(" ")) for ls in train_corpus]
@@ -16,19 +16,24 @@ train_corpus= [list(ls.split(" ")) for ls in train_corpus]
 
 train_corpus = list(create_tagged_document(train_corpus))
 
-with open('drg_clean_preprocessed.txt', 'r', encoding = 'utf8') as file:
-    test_corpus = json.load(file)
+with open('drg_clean_preprocessed_json_UDPipe_manual_correction.txt', 'r', encoding = 'utf8') as file:
+    test_corpus_json = json.load(file)
+    
+test_corpus = []
+for entity in test_corpus_json:
+#    text.append(entity['elements'])
+    test_corpus.append(entity['elements'])
 
 print(train_corpus[:2])
 print(test_corpus[:2])
 sub_test_corpus = test_corpus[-31:-1]
 
-model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+model = gensim.models.doc2vec.Doc2Vec(vector_size = 50, min_count = 5, epochs = 30, workers = 1, dm = 0, dbow = 1, seed = 42)
 model.build_vocab(train_corpus)
 model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
 
 #save model 
-
+model.save("doc2vec_optimed_NZIP.model")
 
 ranks = []
 second_ranks = []
@@ -37,7 +42,6 @@ for doc_id in range(len(train_corpus)):
     sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
     rank = [docid for docid, sim in sims].index(doc_id)
     ranks.append(rank)
-
     second_ranks.append(sims[1])
 
 print('Document ({}): «{}»\n'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
@@ -55,7 +59,6 @@ print('Similar Document {}: «{}»\n'.format(sim_id, ' '.join(train_corpus[sim_i
 
 # Pick a random document from the test corpus and infer a vector from the model
 doc_id = random.randint(0, len(test_corpus) - 1)
-doc_id = 1363
 inferred_vector = model.infer_vector(test_corpus[doc_id])
 sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
 
@@ -63,28 +66,7 @@ sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
 print('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(test_corpus[doc_id])))
 print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
 for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
-    print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
-    
-    #########
-    
-    
-from gensim.models import doc2vec
-from scipy import spatial
+    print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))   
 
-#load model
-#model = doc2vec.Doc2Vec.load(model_file)
-
-doc_id_train = random.randint(0, len(train_corpus) - 1)
-vec1 = model.infer_vector("balintovský skupina diagnostický obor neurověda komunikace sebezkušenost komunikace sebezkušenost cvičení student projít zkušenost bálintovský skupina jakožto efektivní preventivní metoda vůči syndrom vyhoření osobnost praxe psychologie životní zkušenost bálintovský skupina korekce postoj řešení konflikt spor získání náhled charakter vztah zpětný vazba student charakterizovat orientace vztah postoj zpětný vazba korekce postoj".split())
-vec1 = model.infer_vector(["srdecni nemoc", "infarkt"])
-vec1 = model.infer_vector(["plicni nemoc", "dychani"])
-
-
-#0.60967 docID test 1363
-doc_id_test = random.randint(0, len(test_corpus) - 1)
-doc_id_test = 1363
-vec2 = model.infer_vector(test_corpus[doc_id_test])
-
-similairty = 1 - spatial.distance.cosine(vec1, vec2)
-
-test_corpus[1774]
+# save model
+model.save('doc2vec_optimed_NZIP.model')
